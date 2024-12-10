03472ced01a4421bfb46f4df96a10a1ef1cefbb9
Arindam2003 <dindaarindam000@gmail.com>
2024-12-10 23:34:07 +0530

    insert

--- Tree_/binary_search_tree.cpp
+++ Tree_/binary_search_tree.cpp
@@ -18,6 +18,7 @@ public:
     BST();
     bool isempty();
     void insert(int);
+    void preorder();
 };
 
 BST::BST()
@@ -32,7 +33,7 @@ bool BST::isempty()
 
 void BST::insert(int d)
 {
-    node *t = root;
+    node *t;
     node *newnode = new node;
     newnode->data = d;
     newnode->left = nullptr;
@@ -43,26 +44,26 @@ void BST::insert(int d)
     }
     else
     {
-        if (d == t->data)
-            throw DUPLICATE;
         while (t != nullptr)
         {
-            if (d < t->data)
+            if (d < t->data)  // Insert Left Subtree
             {
                 if (t->left == nullptr)
                 {
                     t->left = newnode;
+                    break;
                 }
                 else
                 {
                     t = t->left;
                 }
             }
-            else
+            else  //Insert Right Subtree
             {
                 if (t->right == nullptr)
                 {
                     t->right = newnode;
+                    break;
                 }
                 else
                 {
@@ -70,8 +71,31 @@ void BST::insert(int d)
                 }
             }
         }
+        if(t->data==d)  //Safe for memory leake
+            delete newnode;
     }
 }
+
+
+
+void BST::preorder()
+{
+    node *t=root;
+    if(isempty())
+        cout<<"No value in Tree"<<endl;
+    else
+    {
+        while (t!=nullptr)
+        {
+            cout<<t->data<<"-->";
+            t=t->left;
+
+        }
+        
+    }
+}
+
+// void BST::()`
 
 int main()
 {
