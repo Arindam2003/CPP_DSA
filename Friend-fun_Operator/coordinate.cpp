68675614371e8171be774b21d95303b3b526c6d6
Arindam2003 <dindaarindam000@gmail.com>
2025-01-01 23:22:46 +0530

    Assign_15_complete

--- Friend-fun_Operator/coordinate.cpp
+++ Friend-fun_Operator/coordinate.cpp
@@ -10,4 +10,17 @@ class Coordinate{
         Coordinate operator,(Coordinate C){
             return C;
         }
+        friend ostream& operator<<(ostream& ,Coordinate&);
+        friend istream& operator>>(istream& ,Coordinate&);
 };
+
+ostream& operator<<(ostream& out,Coordinate& t)
+{
+    out<<t.a<<t.b;
+    return out;
+}
+istream& operator>>(istream& in,Coordinate& t)
+{
+    in>>t.a>>t.b;
+    return in;
+}
---
+++ Friend-fun_Operator/student.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+class Student
+{
+private:
+    int roll_no;
+    char name[20];
+    int age;
+public:
+    void setStudent(int r,char n[],int a)
+    {
+        roll_no=r;
+        strcpy(name,n);
+        age=a;
+    }
+
+    void showData()
+    {
+        cout<<"\n"<<roll_no<<" "<<name<<" "<<age;
+    }
+    bool operator==(Student s)
+    {
+        if(roll_no==s.roll_no && strcmp(name,s.name) && age==s.age)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+
